65f3a4524315e49fc599f23c60349da0bbe3e774
kry008 <admin@kodingwww.ml>
2022-12-11 15:43:17 +0100

    Aktualizacja startu

--- Kalkulator/View/Start.xaml.cs
+++ Kalkulator/View/Start.xaml.cs
@@ -5,7 +5,6 @@ public partial class Start : ContentPage
 	public Start()
 	{
 		InitializeComponent();
-        //after 5s go to interfejs.xaml
         Device.StartTimer(TimeSpan.FromSeconds(5), () =>
         {
             Navigation.PushAsync(new Interfejs());
